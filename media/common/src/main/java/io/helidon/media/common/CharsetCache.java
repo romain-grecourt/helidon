34899a914ae6e27b75a335c8537415eb6f73f84a
Romain Grecourt <romain.grecourt@oracle.com>
2019-07-16 15:41:05 -0700

    update class javadoc on CharsetCache

--- media/common/src/main/java/io/helidon/media/common/CharsetCache.java
+++ media/common/src/main/java/io/helidon/media/common/CharsetCache.java
@@ -23,7 +23,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Function;
 
 /**
- * Utility class for caching per commonly used charsets.
+ * Utility for caching per commonly used charsets.
  */
 final class CharsetCache<T> {
 
