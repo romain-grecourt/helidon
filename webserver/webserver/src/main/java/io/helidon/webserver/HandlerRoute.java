114f6e41b622bfdc46fd225a10c299b696e5098f
Romain Grecourt <romain.grecourt@oracle.com>
2020-05-12 17:15:32 -0700

    remove superlfuous change

--- webserver/webserver/src/main/java/io/helidon/webserver/HandlerRoute.java
+++ webserver/webserver/src/main/java/io/helidon/webserver/HandlerRoute.java
@@ -57,7 +57,7 @@ class HandlerRoute implements Route {
         if (methods == null) {
             this.methodPredicate = new HttpMethodPredicate(null);
         } else if (methods instanceof Collection) {
-            this.methodPredicate = new HttpMethodPredicate((Collection<Http.RequestMethod>) methods);
+            this.methodPredicate = new HttpMethodPredicate((Collection) methods);
         } else {
             Collection<Http.RequestMethod> mtds = new ArrayList<>();
             for (Http.RequestMethod method : methods) {
